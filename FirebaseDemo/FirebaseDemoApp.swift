893b592b6a13bdb9165e3e9a4a953b416acc2afa
SrknAdgzl <138037543+SrknAdgzl@users.noreply.github.com>
2023-10-27 17:30:22 +0300

    Initial Commit

---
+++ FirebaseDemo/FirebaseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseDemoApp.swift
+//  FirebaseDemo
+//
+//  Created by Serkan ADIGÜZEL on 27.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
